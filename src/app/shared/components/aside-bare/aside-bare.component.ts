2e0a3d103b8aee532ccc464f297e741ed718bb35
cidi Diallo <booba167967@gmail.com>
2025-09-09 18:39:31 +0000

    before notification mofication

--- src/app/shared/components/aside-bare/aside-bare.component.ts
+++ src/app/shared/components/aside-bare/aside-bare.component.ts
@@ -1,19 +1,36 @@
-import {Component, inject} from '@angular/core';
+import {Component, ElementRef, HostListener, inject} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 
 import {AuthService} from "../../services/auth.service";
 import {SidebarService} from "../../services/sidebar.service";
+import {NgIf} from "@angular/common";
+import {NotificationPopoverComponent} from "../notification-popover/notification-popover.component";
 
 @Component({
   selector: 'app-aside-bare',
   templateUrl: './aside-bare.component.html',
   styleUrls: ['./aside-bare.component.scss'],
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, NgIf, NotificationPopoverComponent],
   standalone: true
 })
 export class AsideBareComponent{
   private authService = inject(AuthService);
   protected sideBareService = inject(SidebarService);
+  private elementRef = inject(ElementRef);
+
+  isPopoverOpen = false;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isPopoverOpen = false;
+    }
+  }
+
+  togglePopover(event: Event): void {
+    event.stopPropagation();
+    this.isPopoverOpen = !this.isPopoverOpen;
+  }
 
    toggleCollapse() {
     this.sideBareService.toggleCollapse();
