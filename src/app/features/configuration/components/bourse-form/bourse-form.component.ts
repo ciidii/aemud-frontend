611e96be9ad6da9bfa66fe4280cdc964654e09f8
ciidii <booba167967@gmail.com>
2025-12-22 15:27:10 +0000

    feat(config): displaying the forms in the modal

--- src/app/features/configuration/components/bourse-form/bourse-form.component.ts
+++ src/app/features/configuration/components/bourse-form/bourse-form.component.ts
@@ -25,8 +25,8 @@ export class BourseFormComponent implements OnInit {
   ngOnInit(): void {
     this.bourseForm = this.fb.group({
       id: [this.bourse?.id],
-      type: [this.bourse?.lebelle || '', [Validators.required, Validators.minLength(3)]],
-      amount: [this.bourse?.montant || null, [Validators.required, Validators.min(1)]]
+      lebelle: [this.bourse?.lebelle || '', [Validators.required, Validators.minLength(3)]],
+      montant: [this.bourse?.montant || null, [Validators.required, Validators.min(1)]]
     });
   }
 
