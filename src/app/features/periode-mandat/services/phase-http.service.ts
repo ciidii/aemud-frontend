173f75a47b2cf6a8124eb931f38c756f2b3a9fae
ciidii <booba167967@gmail.com>
2025-12-24 17:24:09 +0000

    feat(phase): implementing the http methode

--- src/app/features/periode-mandat/services/phase-http.service.ts
+++ src/app/features/periode-mandat/services/phase-http.service.ts
@@ -4,19 +4,31 @@ import {map, Observable} from "rxjs";
 import {ResponseEntityApi} from "../../../core/models/response-entity-api";
 import {environment} from "../../../../environments/environment";
 import {PhaseModel} from "../models/phase.model";
+import {UpdatePhaseModel} from "../models/UpdatePhaseModel";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhaseHttpService {
+  private readonly url = `${environment.API_URL}/phases`;
   http = inject(HttpClient);
 
   public getActivePhase(): Observable<ResponseEntityApi<PhaseModel>> {
-    return this.http.get<ResponseEntityApi<PhaseModel>>(environment.API_URL + '/phases/active');
+    return this.http.get<ResponseEntityApi<PhaseModel>>(`${this.url}/active`);
   }
 
   public getMandatPhases(mandatId: string): Observable<PhaseModel[]> {
-    return this.http.get<ResponseEntityApi<PhaseModel[]>>(`${environment.API_URL}/phases/mandat/${mandatId}`).pipe(
+    return this.http.get<ResponseEntityApi<PhaseModel[]>>(`${this.url}/periode-mandat/${mandatId}`).pipe(
+      map(response => response.data)
+    );
+  }
+
+  public getPhaseById(id: string): Observable<ResponseEntityApi<PhaseModel>> {
+    return this.http.get<ResponseEntityApi<PhaseModel>>(`${this.url}/${id}`);
+  }
+
+  public updatePhase(id: string, payload: UpdatePhaseModel): Observable<PhaseModel> {
+    return this.http.put<ResponseEntityApi<PhaseModel>>(`${this.url}/${id}`, payload).pipe(
       map(response => response.data)
     );
   }
